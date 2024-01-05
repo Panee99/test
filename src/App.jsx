e2c74ffc14c1e88347ba85fa06d0ffb88f1b6d27
Nguyen Minh Hoang <hoangnm191999@gmail.com>
2024-01-05 18:02:29 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,36 @@ import { SpinePlayer } from "@esotericsoftware/spine-player";
 
 export const App = () => {
   const spineRef = useRef(null);
-
+  const spinePlayer = useRef(null);
   useEffect(() => {
-    const spinePlayer = new SpinePlayer("spine", {
+    spinePlayer.current = new SpinePlayer("spine", {
       jsonUrl: "/spine/boom-quest-1.json",
       atlasUrl: "/spine/boom-quest-1.atlas",
       preserveDrawingBuffer: true,
       showControls: false,
       backgroundColor: "#00000000",
       alpha: true,
-      animations: ["idle", "bomb"],
+      
+      // animations: ["idle"],
       success: function () {
         console.log("hello");
       },
     });
 
-    spineRef.current?.appendChild(spinePlayer.canvas);
+    spineRef.current?.appendChild(spinePlayer.current.canvas);
   }, []);
+
+  const click = () => {
+    console.log("click");
+    spinePlayer.current.setAnimation("bomb");
+  };
+
   return (
     <div
       id="spine"
       ref={spineRef}
       style={{ width: "800px", height: "600px" }}
+      onClick={click}
     ></div>
   );
 };
